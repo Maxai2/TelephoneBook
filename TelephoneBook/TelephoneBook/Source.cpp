1b775580cb73cb0591b075e4bdab8a339a8768c7
Maxai <maxali946@gmail.com>
2017-06-23 17:45:35 +0400

    Update Source.cpp
    65%

--- TelephoneBook/TelephoneBook/Source.cpp
+++ TelephoneBook/TelephoneBook/Source.cpp
@@ -8,7 +8,10 @@ HANDLE w = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main()
 {
-	int size = 0;
+	int size = 10;
+
+	Person *pers;
+	pers = (Person*)calloc(size, sizeof(Person));
 
 	SetConsoleCursorPosition(w, { 15, 0 });
 	cout << "PHONE BOOK | COUNT: ";
@@ -21,7 +24,7 @@ int main()
 	SetConsoleTextAttribute(w, 10);
 	cout << "MENU";
 	SetConsoleTextAttribute(w, 7);
-	menu(size);
+	menu(pers, size);
 
 	SetConsoleCursorPosition(w, { 0, 23 });
 	cout << endl;
