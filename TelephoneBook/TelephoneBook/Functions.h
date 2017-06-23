ad2747b4e296a8653af38c984a4bc2041a821cef
Maxai <maxali946@gmail.com>
2017-06-23 17:45:03 +0400

    Update Functions.h
    65%

--- TelephoneBook/TelephoneBook/Functions.h
+++ TelephoneBook/TelephoneBook/Functions.h
@@ -1,4 +1,21 @@
 #pragma once
 
+#define nameSurname 50
+#define mail 60
+#define phoneNumber 20
+//#define ageNumber 3
+
+struct Person
+{
+	char name[nameSurname];
+//	char *name = new char[nameSurname];  
+//	char *name = (char*)calloc(nameSurname, sizeof(char));
+	char surname[nameSurname] = {};
+	//char age[ageNumber];
+	int age = 0;
+	char number[phoneNumber] = {};
+	char email[mail] = {};
+};
+
 void frame();
-void menu(int size);
+void menu(Person *pers, int size);
